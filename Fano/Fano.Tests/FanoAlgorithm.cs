8af58f7e91a1f273df9fd6fa9a9a09a393d50cca
Gedas Liaudanskis <g.liaudanskis@gmail.com>
2021-06-20 19:57:04 +0300

    adding methods for shanon-fano algorithm

--- Fano/Fano.Tests/FanoAlgorithm.cs
+++ Fano/Fano.Tests/FanoAlgorithm.cs
@@ -5,47 +5,66 @@ namespace Fano.Tests
     [TestClass]
     public class FanoAlgorithmTest
     {
+        private Parser parser;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestFileUtilities.MakeFile("aaaabbbbccccddee");
+
+            int bitsWordLenght = 3;
+            parser = new Parser(TestFileUtilities.path, bitsWordLenght);
+        }
+
         [TestCleanup]
         public void TestClean() => TestFileUtilities.DeleteFile();
 
         [TestMethod]
         public void TotalFrequency_Table9866643_CorrectSum()
         {
-            TestFileUtilities.MakeFile("aaaabbbbccccddee");
-
-            int bitsWordLenght = 3;
-            var parser = new Parser(TestFileUtilities.path, bitsWordLenght);
-            //{ 9, 8, 6, 6, 6, 4, 3 };
-
+            var expected = new int[] { 23, 18, 13, 42, 19, 7, 18 };
 
             parser.SetFrequencyTable();
             FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());           
 
-            Assert.AreEqual(23, fano.TotalFrequency(0, 2));
-            Assert.AreEqual(18, fano.TotalFrequency(2, 4));
-            Assert.AreEqual(13, fano.TotalFrequency(4, 6));
-            Assert.AreEqual(42, fano.TotalFrequency(0, 6));
-            Assert.AreEqual(19, fano.TotalFrequency(3, 6));
-            Assert.AreEqual(7, fano.TotalFrequency(5, 6));
-            Assert.AreEqual(18, fano.TotalFrequency(2, 4));
+            Assert.AreEqual(expected[0], fano.TotalFrequency(0, 2));
+            Assert.AreEqual(expected[1], fano.TotalFrequency(2, 4));
+            Assert.AreEqual(expected[2], fano.TotalFrequency(4, 6));
+            Assert.AreEqual(expected[3], fano.TotalFrequency(0, 6));
+            Assert.AreEqual(expected[4], fano.TotalFrequency(3, 6));
+            Assert.AreEqual(expected[5], fano.TotalFrequency(5, 6));
+            Assert.AreEqual(expected[6], fano.TotalFrequency(2, 4));
         }
 
         [TestMethod]
         public void SplitIndex_Table9866643_CorrectIndex()
         {
-            TestFileUtilities.MakeFile("aaaabbbbccccddee");
-            
-            int bitsWordLenght = 3;
-            var parser = new Parser(TestFileUtilities.path, bitsWordLenght);
-            //Frequencies { 9, 8, 6, 6, 6, 4, 3 };
+            var expected = new int[] { 2, 4, 5, 2 };
 
             parser.SetFrequencyTable();
             FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());
   
-            Assert.AreEqual(2, fano.SplitIndex(0, 6));
-            Assert.AreEqual(4, fano.SplitIndex(3, 6));
-            Assert.AreEqual(5, fano.SplitIndex(5, 6));
-            Assert.AreEqual(2, fano.SplitIndex(2, 4));
+            Assert.AreEqual(expected[0], fano.SplitIndex(0, 6));
+            Assert.AreEqual(expected[1], fano.SplitIndex(3, 6));
+            Assert.AreEqual(expected[2], fano.SplitIndex(5, 6));
+            Assert.AreEqual(expected[3], fano.SplitIndex(2, 4));
+        }
+
+        [TestMethod]
+        public void GetIntFroomBitArray_FrequencyTable_CorrectConversion()
+        {            
+            var expected = new int[] { 1, 3, 0, 6, 4, 2, 5 };
+
+            parser.SetFrequencyTable();
+            FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());
+
+            Assert.AreEqual(expected[0], fano.GetIntFromBitArray(0));
+            Assert.AreEqual(expected[1], fano.GetIntFromBitArray(1));
+            Assert.AreEqual(expected[2], fano.GetIntFromBitArray(2));
+            Assert.AreEqual(expected[3], fano.GetIntFromBitArray(3));
+            Assert.AreEqual(expected[4], fano.GetIntFromBitArray(4));
+            Assert.AreEqual(expected[5], fano.GetIntFromBitArray(5));
+            Assert.AreEqual(expected[6], fano.GetIntFromBitArray(6));
         }
     }
 }
--- Fano/Fano/FanoAlgorithm.cs
+++ Fano/Fano/FanoAlgorithm.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 
 namespace Fano
 {
     public class FanoAlgorithm
     {
-       private List<WordFrequency> table;
+        private readonly List<WordFrequency> table;
+        private Dictionary<int, BitArray> codes;
 
         public FanoAlgorithm(List<WordFrequency> table)
         {
             this.table = table;
+            codes = new Dictionary<int, BitArray>();
+
+            InitializeDictionary();
         }
 
         public void Start(int left, int right)
-        {
-            int index = SplitIndex(left, right);            
-
+        {              
             if (left == right)
             {
                 return;
+            }             
+
+            if (left == right - 1)
+            {
+                CodeAppendBit(left, false);
+                CodeAppendBit(right, true);
+
+                return;
             }
 
-            //generating word - coders dictionary;
+            int index = SplitIndex(left, right);
+
+            for (int i = left; i <= index; i++)
+            {
+                CodeAppendBit(i, false);
+            }
+
+            for (int i = index + 1; i <= right; i++)
+            {
+                CodeAppendBit(right, true);
+            }          
 
             Start(left, index);
             Start(index + 1, right);
@@ -44,23 +65,49 @@ namespace Fano
         {
             int maxArraySum = TotalFrequency(left, right);
 
-            int index = left - 1;
+            int index = left;
 
             int arraySum = 0;
             int nextArraySum, difference, nextDifference;
 
             do
             {
-                index++;
-
                 arraySum += table[index].Frequency;
                 nextArraySum = arraySum + table[index + 1].Frequency;
 
                 difference = Math.Abs(arraySum - (maxArraySum - arraySum));
-                nextDifference = Math.Abs(nextArraySum - (maxArraySum - nextArraySum));                
+                nextDifference = Math.Abs(nextArraySum - (maxArraySum - nextArraySum));
+
+                index++;
             } while (nextDifference < difference);
 
-            return index;
+            return index - 1;
+        }
+
+        private void InitializeDictionary()
+        {
+            for (int i = 0; i < table.Count; i++)
+            {
+                codes.Add(GetIntFromBitArray(i), null);
+            } 
+        }
+
+        public int GetIntFromBitArray(int index)
+        {            
+            int sum = 0;
+
+            foreach (bool bit in table[index].Word)
+            {
+                sum = sum * 2 + (bit ? 1 : 0);
+            }
+
+            return sum;
+        }
+
+        public void CodeAppendBit(int index, bool value)
+        {
+            codes[GetIntFromBitArray(index)].Length++;
+            codes[GetIntFromBitArray(index)].Set(codes[GetIntFromBitArray(index)].Length - 1, value);
         }
     }
 }

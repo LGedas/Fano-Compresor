3d5b11be255ffee5d86d6bbcd4550ca14a3597d4
Gedas Liaudanskis <g.liaudanskis@gmail.com>
2021-06-22 11:10:37 +0300

    refactored FanoAlgorithm class and unit test files

--- Fano/Fano.Tests/FanoAlgorithm.cs
+++ Fano/Fano.Tests/FanoAlgorithm.cs
@@ -23,56 +23,6 @@ namespace Fano.Tests
         public void TestClean() => TestFileUtilities.DeleteFile();
 
         [TestMethod]
-        public void TotalFrequency_Table9866643_CorrectSum()
-        {
-            var expected = new int[] { 23, 18, 13, 42, 19, 7, 18 };
-
-            parser.SetFrequencyTable();
-            FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());           
-
-            Assert.AreEqual(expected[0], fano.TotalFrequency(0, 2));
-            Assert.AreEqual(expected[1], fano.TotalFrequency(2, 4));
-            Assert.AreEqual(expected[2], fano.TotalFrequency(4, 6));
-            Assert.AreEqual(expected[3], fano.TotalFrequency(0, 6));
-            Assert.AreEqual(expected[4], fano.TotalFrequency(3, 6));
-            Assert.AreEqual(expected[5], fano.TotalFrequency(5, 6));
-            Assert.AreEqual(expected[6], fano.TotalFrequency(2, 4));
-        }
-
-        [TestMethod]
-        public void SplitIndex_Table9866643_CorrectIndex()
-        {
-            var expected = new int[] { 2, 0, 4, 1, 3, 5 };
-
-            parser.SetFrequencyTable();
-            FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());
-  
-            Assert.AreEqual(expected[0], fano.SplitIndex(0, 6));
-            Assert.AreEqual(expected[1], fano.SplitIndex(0, 2));
-            Assert.AreEqual(expected[2], fano.SplitIndex(3, 6));
-            Assert.AreEqual(expected[3], fano.SplitIndex(1, 2));
-            Assert.AreEqual(expected[4], fano.SplitIndex(3, 4));                 
-            Assert.AreEqual(expected[5], fano.SplitIndex(5, 6));
-        }
-
-        [TestMethod]
-        public void GetIntFroomBitArray_FrequencyTable_CorrectConversion()
-        {            
-            var expected = new int[] { 1, 3, 0, 6, 4, 2, 5 };
-
-            parser.SetFrequencyTable();
-            FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());
-
-            Assert.AreEqual(expected[0], fano.GetIntFromBitArray(0));
-            Assert.AreEqual(expected[1], fano.GetIntFromBitArray(1));
-            Assert.AreEqual(expected[2], fano.GetIntFromBitArray(2));
-            Assert.AreEqual(expected[3], fano.GetIntFromBitArray(3));
-            Assert.AreEqual(expected[4], fano.GetIntFromBitArray(4));
-            Assert.AreEqual(expected[5], fano.GetIntFromBitArray(5));
-            Assert.AreEqual(expected[6], fano.GetIntFromBitArray(6));
-        }
-
-        [TestMethod]
         public void Start_FrequencyTable_CorrectDictionary()
         {            
             var expectedKeys = new int[] { 1, 3, 0, 6, 4, 2, 5 };
@@ -89,25 +39,25 @@ namespace Fano.Tests
 
             parser.SetFrequencyTable();
             FanoAlgorithm fano = new FanoAlgorithm(parser.GetFrequencyTable());
-            fano.Start();  
+            Dictionary<int, BitArray> bitsByInt = fano.GetBitsByInt();
 
-            Assert.AreEqual(expectedKeys.Count(), fano.BitsByInt.Count);
+            Assert.AreEqual(expectedKeys.Count(), bitsByInt.Count);
 
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[0]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[1]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[2]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[3]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[4]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[5]));
-            Assert.IsTrue(fano.BitsByInt.ContainsKey(expectedKeys[6]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[0]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[1]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[2]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[3]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[4]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[5]));
+            Assert.IsTrue(bitsByInt.ContainsKey(expectedKeys[6]));
 
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[0], fano.BitsByInt[expectedKeys[0]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[1], fano.BitsByInt[expectedKeys[1]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[2], fano.BitsByInt[expectedKeys[2]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[3], fano.BitsByInt[expectedKeys[3]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[4], fano.BitsByInt[expectedKeys[4]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[5], fano.BitsByInt[expectedKeys[5]]));
-            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[6], fano.BitsByInt[expectedKeys[6]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[0], bitsByInt[expectedKeys[0]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[1], bitsByInt[expectedKeys[1]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[2], bitsByInt[expectedKeys[2]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[3], bitsByInt[expectedKeys[3]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[4], bitsByInt[expectedKeys[4]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[5], bitsByInt[expectedKeys[5]]));
+            Assert.IsTrue(TestUtils.AreBitArraysEqual(expectedBits[6], bitsByInt[expectedKeys[6]]));
         }
     }
 }
--- Fano/Fano/FanoAlgorithm.cs
+++ Fano/Fano/FanoAlgorithm.cs
@@ -8,23 +8,25 @@ namespace Fano
     public class FanoAlgorithm
     {
         private readonly List<WordFrequency> wordFrequency;
-        private Dictionary<int, BitArray> _bitsByInt;
+        private Dictionary<int, BitArray> bitsByInt;
         private readonly int[] keys;
 
         public FanoAlgorithm(List<WordFrequency> wordFrequency)
         {
             this.wordFrequency = wordFrequency;
-            _bitsByInt = new Dictionary<int, BitArray>();
-            keys = new int [wordFrequency.Count];
+            bitsByInt = new Dictionary<int, BitArray>();            
 
-            _bitsByInt = Enumerable.Range(0, wordFrequency.Count).ToDictionary(x => GetIntFromBitArray(x), x => (BitArray)null);
-            keys = Enumerable.Range(0, wordFrequency.Count).Select(x => GetIntFromBitArray(x)).ToArray();         
+            bitsByInt = Enumerable.Range(0, wordFrequency.Count).ToDictionary(x => GetIntFromBitArray(x), x => (BitArray)null); 
+            keys = bitsByInt.Select(x => x.Key).ToArray();
+        }
+       
+        public Dictionary<int, BitArray> GetBitsByInt() 
+        {
+            GenerateBitsByInt(0, bitsByInt.Count - 1);
+            return bitsByInt;
         }
 
-        //TO DO: write a better name.
-        public void Start() => GetBitsByInt(0, _bitsByInt.Count - 1);
-
-        public void GetBitsByInt(int left, int right)
+        private void GenerateBitsByInt(int left, int right)
         {
             if (left == right)
             {
@@ -35,58 +37,55 @@ namespace Fano
 
             AddBits(left, index, right);
 
-            GetBitsByInt(left, index);
-            GetBitsByInt(index + 1, right);
+            GenerateBitsByInt(left, index);
+            GenerateBitsByInt(index + 1, right);
         }
 
-        //TO DO: needs a better name.
-        public void AddBits(int left, int index, int right)
+        private void AddBits(int left, int index, int right)
         {  
             for (int i = left; i <= right; i++)
             {
-                bool value = (i <= index) ? false : true;
-                CodeAppendBit(i, value);                 
+                bool value = i <= index ? false : true;
+                AddBit(i, value);                 
             }
         }
 
-        //TO DO: needs a better name.
-        public void CodeAppendBit(int index, bool value)
+        private void AddBit(int index, bool value)
         {
-            if (_bitsByInt[keys[index]] == null)
+            if (bitsByInt[keys[index]] == null)
             {
-                _bitsByInt[keys[index]] = new BitArray(new[] { value });
+                bitsByInt[keys[index]] = new BitArray(new[] { value });
                 return;
             }
 
-            _bitsByInt[keys[index]].Length++;
-            _bitsByInt[keys[index]].Set(_bitsByInt[keys[index]].Length - 1, value);
+            bitsByInt[keys[index]].Length++;
+            bitsByInt[keys[index]].Set(bitsByInt[keys[index]].Length - 1, value);
         }
 
-        public int SplitIndex(int left, int right)
+        private int SplitIndex(int left, int right)
         {
             int maxArraySum = TotalFrequency(left, right);
-
-            int index = left;
             int arraySum = 0;
-            int nextArraySum = 0;
-            int difference = 0;
-            int nextDifference = 0;
 
-            do
+            for (int index = left; index < right; index++)
             {
                 arraySum += wordFrequency[index].Frequency;
-                nextArraySum = arraySum + wordFrequency[index + 1].Frequency;
+                int nextArraySum = arraySum + wordFrequency[index + 1].Frequency;
 
-                difference = Math.Abs(arraySum - (maxArraySum - arraySum));
-                nextDifference = Math.Abs(nextArraySum - (maxArraySum - nextArraySum));
+                int difference = Math.Abs(arraySum - (maxArraySum - arraySum));
+                int nextDifference = Math.Abs(nextArraySum - (maxArraySum - nextArraySum));
 
-                index++;
-            } while (nextDifference < difference);
+                if (difference <= nextDifference)
+                {
+                    return index;
+                }
+            }
 
-            return index - 1;
+            //did not found split index
+            throw new Exception();
         }
 
-        public int TotalFrequency(int left, int right)
+        private int TotalFrequency(int left, int right)
         {
             int sum = 0;
 
@@ -98,8 +97,7 @@ namespace Fano
             return sum;
         }
 
-        //TO DO: needs a better name.
-        public int GetIntFromBitArray(int index)
+        private int GetIntFromBitArray(int index)
         {
             int sum = 0;
 
@@ -109,8 +107,6 @@ namespace Fano
             }
 
             return sum;
-        }  
-
-        public Dictionary<int, BitArray> BitsByInt => _bitsByInt;        
+        } 
     }
 }
